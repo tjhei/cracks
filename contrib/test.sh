d0e5fe7921f2b61302cf7a46251b59b04b675f29
Timo Heister <timo.heister@gmail.com>
2018-11-10 22:58:54 -0500

    CI: new deal.II images

--- contrib/test.sh
+++ contrib/test.sh
@@ -11,7 +11,7 @@ echo "checking indentation ..."
 docker run \
        --rm \
        -v `pwd`:/source \
-       dealii/dealii:v8.5.1-gcc-mpi-fulldepscandi-debugrelease \
+       tjhei/dealii:v8.5.1-full-v8.5.1-r1 \
        bash -c "cd /source && ./contrib/indent && git diff --exit-code" \
     || { echo "Please check indentation!"; exit 1; }
 echo "    OK"
@@ -20,7 +20,7 @@ echo "checking deal.II v8.5.1 ..."
 docker run \
        --rm \
        -v `pwd`:/source \
-       dealii/dealii:v8.5.1-gcc-mpi-fulldepscandi-debugrelease \
+       tjhei/dealii:v8.5.1-full-v8.5.1-r1 \
        bash -c "mkdir build; cd build; cmake /source && make -j 4 && ./cracks" \
     || { echo "Failed!"; exit 1; }
 echo "    OK"
@@ -29,7 +29,7 @@ echo "checking deal.II v9.0.0 ..."
 docker run \
        --rm \
        -v `pwd`:/source \
-       dealii/dealii:v9.0.0-gcc-mpi-fulldepscandi-debugrelease \
+       tjhei/dealii:v9.0.1-full-v9.0.1-r3 \
        bash -c "mkdir build; cd build; cmake -D CMAKE_CXX_FLAGS='-Werror' /source && make -j 4 && ./cracks && ctest -j 4" \
     || { echo "Failed!"; exit 1; }
 echo "    OK"
